c304e032a76a66a90af1d5e998d3bf3cb19b77f3
Ian Chu Te <ianhervechute@gmail.com>
2017-05-11 16:32:27 +0800

    Boolean binding.

--- js/teradata.js
+++ js/teradata.js
@@ -1,55 +1,52 @@
 const app = angular.module('teradata.module', []);
 
 app.controller('TeradataController', function ($scope) {
-	
-  var functionMetadata;
-	
-  $scope.getFunctionMetadata = function(selectedFunction)
-  {
-	  console.log('getfunctionmetadata');
-	  $http.get('/plugins/AsterAnalytics/resource/data/' + selectedFunction + '.json')
-      .success(function(data) {
-          $scope.contents=data;
-          functionMetadata = data;
-      })
-      .error(function(data,status,error,config){
-          $scope.contents = [{heading:"Error",description:"Could not load json data"}];
-          console.log($scope.contents);
+
+  let functionMetadata;
+
+  $scope.getFunctionMetadata = function (selectedFunction) {
+
+    if (typeof selectedFunction === 'undefined') return;
+
+    $http.get('/plugins/AsterAnalytics/resource/data/' + selectedFunction + '.json')
+      .success(function (data) {
+        $scope.contents = data;
+        functionMetadata = data;
+      }).error(function (data, status, error, config) {
+        $scope.contents = [{
+          heading: "Error",
+          description: "Could not load json data"
+        }];
       });
-	  
   }
-  
-  $scope.getFunctionDescription = function()
-  {
-	  if (functionMetadata && 'long_description' in functionMetadata)
-	  {
-		  return functionMetadata.long_description;
-	  }
-	  return "";
+
+  $scope.getFunctionDescription = function () {
+    return functionMetadata && ('long_description' in functionMetadata) ?
+      functionMetadata.long_description :
+      '';
   }
-	
-  $scope.getArgumentDescription = function(selectedFunction, functionArgument)
-  {
-	  var description = "";
-	      if (functionMetadata && functionMetadata.argument_clauses)
-	    	  {
-	    	  
-	          var functionargumententry = functionMetadata.argument_clauses.filter(function(item) {
-	        	  if ('alternateNames' in item)
-	        	  {
-	        		  return (item.alternateNames.map(function(x){ return x.toUpperCase() }).indexOf(functionArgument.toUpperCase()) > -1)  
-	        	  }
-	        	  return (item.name.toUpperCase() === functionArgument.toUpperCase());
-	        	});
-	          if (functionargumententry && 0 < functionArgument.length)
-	          {
-	            
-	            description = functionargumententry[0].description;
-	          }
-	          
-	      }
-	      
-		  return description;
+
+  $scope.getArgumentDescription = function (selectedFunction, functionArgument) {
+    let description = '';
+    if (functionMetadata && functionMetadata.argument_clauses) {
+
+      const functionargumententry = functionMetadata.argument_clauses.filter(function (item) {
+        if ('alternateNames' in item) {
+          return item.alternateNames
+            .map(x => x.toUpperCase())
+            .indexOf(functionArgument.toUpperCase()) > -1;
+        } else {
+          return item.name.toUpperCase() === functionArgument.toUpperCase();
+        }
+      });
+
+      if (functionargumententry && functionargumententry.length > 0) {
+        description = functionargumententry[0].description;
+      }
+
+    }
+
+    return description;
   };
 
   $scope.callPythonDo({}).then(
@@ -58,9 +55,6 @@ app.controller('TeradataController', function ($scope) {
       $scope.schema = data.schema;
       $scope.inputs = data.inputs;
       console.log(data);
-      console.log(window.location.pathname);
-      
-      
 
       $('select:first').change(() => $('#tabs').tabs());
       $('select:first, select:first > option').css('text-transform', 'capitalize');
@@ -71,11 +65,10 @@ app.controller('TeradataController', function ($scope) {
       $scope.inputs = [];
       console.log(data);
     }
-    
-    
+
   );
-  
-  
+
+
 
   setTimeout(() => {
 
