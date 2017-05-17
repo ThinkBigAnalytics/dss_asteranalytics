c78cc73e503f16f03f70cef861db5a7f08ead4d2
Teodoro <donnaapril.teodoro@teradata.com>
2017-05-17 17:23:28 +0800

    test EvaluateNamedEntityFinderPartition

--- python-lib-tests/testTextAnalysis.py
+++ python-lib-tests/testTextAnalysis.py
@@ -10,6 +10,7 @@ from testTextAnalysisConfig import *
 from inputtableinfo import *
 from outputtableinfo import *
 from connectioninfo import *
+from dssFunction import *
 
 class TestTextAnalysis(unittest.TestCase):
 
@@ -39,4 +40,30 @@ TEXTCOLUMN('chunk')
 COMMIT;
 END TRANSACTION;"""
         self.assertEqual(actualquery, expectedquery, "Ohno")
+        
+    def testEvaluateNamedEntityFinderPartition(self):
+        testInputConnectionConfig = {'table' : 'textclassifier_input', 'schema':'dss'}
+        testOuputConnectionConfig = {'table' : 'dss_test'}
+        testfunction = dssFunction()
+        testConfig = testfunction.name('EVALUATENAMEDENTITYFINDERPARTITION').unaliased_inputs_count(0).add_unaliased_input('textclassifier_input', 'PartitionByOne').build()
+        functionInputTable = inputtableinfo(testInputConnectionConfig, 'textclassifier_input', testConfig)
+        functionOutputTable = outputtableinfo(testOuputConnectionConfig, 'dss_test', testConfig)
+        actualquery = getFunctionsQuery(testConfig, [functionInputTable], functionOutputTable)
+        expectedquery = """BEGIN TRANSACTION;
+DROP TABLE IF EXISTS public.dss_test;
+CREATE DIMENSION TABLE public.dss_test
+AS 
+SELECT *
+FROM   EVALUATENAMEDENTITYFINDERPARTITION
+(
+
+ON dss.textclassifier_input PARTITION BY 1
+
+
+
+);
+COMMIT;
+END TRANSACTION;"""
+        self.assertEqual(actualquery, expectedquery, 'EvaluateNamedEntityFinderPartition')
+        
         
