8edebc1771562370ddb0bf7553ba54e5cd9556ce
Teodoro <donnaapril.teodoro@thinkbiganalytics.com>
2017-09-11 19:25:38 +0800

    [bug fix] delete multiple output tables if exists

--- python-lib/querybuilderfacade.py
+++ python-lib/querybuilderfacade.py
@@ -17,9 +17,18 @@ def getCreateQuery(dss_function, inputTables, outputTable):
                        outputTable.hashKey and DISTRIBUTE_BY_HASH.format(outputTable.hashKey),
                        getSelectClause(dss_function, inputTables))
 
+def getDropOutputTableArgumentsStatementFromMultipleArguments(arg):
+    outputs = arg.get('value', '').split(',')
+    return '\n'.join(DROP_QUERY.format(outputTableName=x)\
+            for x in outputs)
+
 def getDropOutputTableArgumentsStatements(args):
     return ['BEGIN TRANSACTION;'] + [DROP_QUERY.format(outputTablename=x.get('value', ''))\
-            for x in args if x.get('isOutputTable', False) and x.get('value','')] + ['COMMIT;']
+            for x in args if x.get('isOutputTable', False) and not x.get('allowsLists', False)\
+            and x.get('value','')] + [getDropOutputTableArgumentsStatementFromMultipleArguments(x)\
+                                      for x in args if x.get('isOutputTable', False)\
+                                      and x.get('allowsLists', False) and x.get('value', '')]\
+                                       + ['COMMIT;']
 
 def getBeginDropCreateQueries(dss_function, inputTables, outputTable):
      return [BEGIN_TRANSACTION_QUERY,
